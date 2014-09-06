c822ae5222e9b60ab892255399b8d58db425c40f
InDey <hokuspokras16@gmail.com>
2014-09-06 13:56:38 -0400

    menu systems basically work

--- TwoDeeFighter/Assets/MyAssets/Scripts/SelectionManager.cs
+++ TwoDeeFighter/Assets/MyAssets/Scripts/SelectionManager.cs
@@ -17,11 +17,17 @@ public class SelectionManager : MonoBehaviour
     public const string KEY_A = "KeyA";
 
     public const int NUM_CHARS = 4;
+    public int num_Players_selected = 0;
 
     public Transform[] P1Pos = new Transform[NUM_CHARS];
     public Transform[] P2Pos = new Transform[NUM_CHARS];
     public Transform[] P3Pos = new Transform[NUM_CHARS];
     public Transform[] P4Pos = new Transform[NUM_CHARS];
+
+    public bool p1CanMove = true;
+    public bool p2CanMove = true;
+    public bool p3CanMove = true;
+    public bool p4CanMove = true;
 
     public class playerChoice
     {
@@ -42,6 +48,9 @@ public class SelectionManager : MonoBehaviour
 
     void Awake()
     {
+        num_Players_selected = 0;
+        //TODO make these start at the starting position until you move
+
         // P1 setup
         P1choice = new playerChoice();
         P1choice.icon = GameObject.FindGameObjectWithTag("P1");
@@ -69,29 +78,77 @@ public class SelectionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("number players selected: " + num_Players_selected);
         // Select Characters
-        if (Input.GetButtonDown(P1 + KEY_O))
+        if (Input.GetButtonDown(P1 + KEY_O) && p1CanMove)
         {
+            p1CanMove = false;
             P1choice.finalChoice = selectCharacter(P1choice.index);
             Debug.Log("P1 selected" + P1choice.finalChoice.name);
+            PlayerPrefs.SetInt("p1Choice", P1choice.index);
+            num_Players_selected++;
         }
-        if (Input.GetButtonDown(P2 + KEY_O))
+        if (Input.GetButtonDown(P2 + KEY_O) && p2CanMove)
         {
+            p2CanMove = false;
             P2choice.finalChoice = selectCharacter(P2choice.index);
             Debug.Log("P2 selected" + P2choice.finalChoice.name);
+            PlayerPrefs.SetInt("p2Choice", P2choice.index);
+            num_Players_selected++;
         }
-        if (Input.GetButtonDown(P3 + KEY_O))
+        if (Input.GetButtonDown(P3 + KEY_O) && p3CanMove)
         {
+            p3CanMove = false;
             P3choice.finalChoice = selectCharacter(P3choice.index);
             Debug.Log("P3 selected" + P3choice.finalChoice.name);
+            PlayerPrefs.SetInt("p3Choice", P3choice.index);
+            num_Players_selected++;
         }
-        if (Input.GetButtonDown(P4 + KEY_O))
+        if (Input.GetButtonDown(P4 + KEY_O) && p4CanMove)
         {
+            p4CanMove = false;
             P4choice.finalChoice = selectCharacter(P4choice.index);
             Debug.Log("P4 selected" + P4choice.finalChoice.name);
+            PlayerPrefs.SetInt("p4Choice", P4choice.index);
+            num_Players_selected++;
         }
+
+        // canceling selection
+        if (Input.GetButtonDown(P1 + KEY_A) && !p1CanMove)
+        {
+            p1CanMove = true;
+            P1choice.finalChoice = null;
+            num_Players_selected--;
+        }
+        if (Input.GetButtonDown(P2 + KEY_A) && !p2CanMove)
+        {
+            p2CanMove = true;
+            P2choice.finalChoice = null;
+            num_Players_selected--;
+        }
+        if (Input.GetButtonDown(P3 + KEY_A) && !p3CanMove)
+        {
+            p3CanMove = true;
+            P3choice.finalChoice = null;
+            num_Players_selected--;
+        }
+        if (Input.GetButtonDown(P4 + KEY_A) && !p4CanMove)
+        {
+            p4CanMove = true;
+            P4choice.finalChoice = null;
+            num_Players_selected--;
+        }
+
+        // START GAME
+        if (Input.GetButtonUp("Start") && (num_Players_selected > 1))
+        {
+            
+            Application.LoadLevel("Scene1");
+        }
+
     }
 
+    // Select character method
     public GameObject selectCharacter(int i)
     {
         switch (i)
@@ -119,21 +176,22 @@ public class SelectionManager : MonoBehaviour
         }
     }
 
+
     void moveCharacter()
     {
-        if (Input.GetButton(P1 + HORIZ))
+        if (Input.GetButton(P1 + HORIZ) && p1CanMove)
         {
             reposition(P1, P1choice, P1Pos);
         }
-        if (Input.GetButton(P2 + HORIZ))
+        if (Input.GetButton(P2 + HORIZ) && p2CanMove)
         {
             reposition(P2, P2choice, P2Pos);
         }
-        if (Input.GetButton(P3 + HORIZ))
+        if (Input.GetButton(P3 + HORIZ) && p3CanMove)
         {
             reposition(P3, P3choice, P3Pos);
         }
-        if (Input.GetButton(P4 + HORIZ))
+        if (Input.GetButton(P4 + HORIZ) && p4CanMove)
         {
             reposition(P4, P4choice, P4Pos);
         }
