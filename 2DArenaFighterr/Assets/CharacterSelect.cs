ef05a603883b0e03ad10ee9ea88a0755db29da2f
Alex Good <alexgoodproductions@gmail.com>
2014-09-06 04:05:22 -0400

    Title and character select started

---
+++ 2DArenaFighterr/Assets/CharacterSelect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelect : MonoBehaviour {
+
+	public GameObject[] characters;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
