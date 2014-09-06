c2a20629632adc82a5b34e2329ff12f91381fc3b
InDey <hokuspokras16@gmail.com>
2014-09-06 19:37:23 -0400

    barons anamator

--- TwoDeeFighter/Assets/MyAssets/Scripts/GameManager.cs
+++ TwoDeeFighter/Assets/MyAssets/Scripts/GameManager.cs
@@ -14,51 +14,51 @@ public class GameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		if (PlayerPrefs.HasKey("p1Choice"))
-		{
-			int p1Choice = PlayerPrefs.GetInt("p1Choice");
-            Debug.Log("P1 has a choice of:" + p1Choice);
-			if (p1Choice >= 0) {
-				GameObject player1 = (GameObject) Instantiate(player, transform.position, transform.rotation);
-				Character character = addCharacter (player1, p1Choice);
-				character.setPlayerNumber (0);
-				Players.Add (player1);
-			}
-		}
+        //if (PlayerPrefs.HasKey("p1Choice"))
+        //{
+        //    int p1Choice = PlayerPrefs.GetInt("p1Choice");
+        //    Debug.Log("P1 has a choice of:" + p1Choice);
+        //    if (p1Choice >= 0) {
+        //        GameObject player1 = (GameObject) Instantiate(player, transform.position, transform.rotation);
+        //        Character character = addCharacter (player1, p1Choice);
+        //        character.setPlayerNumber (0);
+        //        Players.Add (player1);
+        //    }
+        //}
 
-		if (PlayerPrefs.HasKey("p2Choice"))
-		{
-			int p2Choice = PlayerPrefs.GetInt("p2Choice");
-            Debug.Log("P2 has a choice of:" + p2Choice);
-			if (p2Choice >= 0) {
-				GameObject player2 = (GameObject) Instantiate(player, transform.position, transform.rotation);
-				Character character = addCharacter (player2, p2Choice);
-				character.setPlayerNumber (1);
-				Players.Add (player2);
-			}
-		}
+        //if (PlayerPrefs.HasKey("p2Choice"))
+        //{
+        //    int p2Choice = PlayerPrefs.GetInt("p2Choice");
+        //    Debug.Log("P2 has a choice of:" + p2Choice);
+        //    if (p2Choice >= 0) {
+        //        GameObject player2 = (GameObject) Instantiate(player, transform.position, transform.rotation);
+        //        Character character = addCharacter (player2, p2Choice);
+        //        character.setPlayerNumber (1);
+        //        Players.Add (player2);
+        //    }
+        //}
 
-		if (PlayerPrefs.HasKey("p3Choice"))
-		{
-			int p3Choice = PlayerPrefs.GetInt("p3Choice");
-			if (p3Choice >= 0) {
-				GameObject player3 = (GameObject) Instantiate(player, transform.position, transform.rotation);
-				Character character = addCharacter (player3, p3Choice);
-				character.setPlayerNumber (2);
-				Players.Add (player3);
-			}
-		}
+        //if (PlayerPrefs.HasKey("p3Choice"))
+        //{
+        //    int p3Choice = PlayerPrefs.GetInt("p3Choice");
+        //    if (p3Choice >= 0) {
+        //        GameObject player3 = (GameObject) Instantiate(player, transform.position, transform.rotation);
+        //        Character character = addCharacter (player3, p3Choice);
+        //        character.setPlayerNumber (2);
+        //        Players.Add (player3);
+        //    }
+        //}
 
-		if (PlayerPrefs.HasKey("p4Choice"))
-		{
-			int p4Choice = PlayerPrefs.GetInt("p4Choice");
-			if (p4Choice >= 0) {
-				GameObject player4 = (GameObject) Instantiate(player, transform.position, transform.rotation);
-				Character character = addCharacter (player4, p4Choice);
-				character.setPlayerNumber (3);
-				Players.Add (player4);
-			}
-		}
+        //if (PlayerPrefs.HasKey("p4Choice"))
+        //{
+        //    int p4Choice = PlayerPrefs.GetInt("p4Choice");
+        //    if (p4Choice >= 0) {
+        //        GameObject player4 = (GameObject) Instantiate(player, transform.position, transform.rotation);
+        //        Character character = addCharacter (player4, p4Choice);
+        //        character.setPlayerNumber (3);
+        //        Players.Add (player4);
+        //    }
+        //}
 	}
 
 	private Character addCharacter(GameObject player, int character) 
